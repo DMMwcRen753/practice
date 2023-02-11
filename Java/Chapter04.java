68fae0444d20f9490e3646b6b686a529f7809289
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-11 10:56:55 +0000

    変数について

---
+++ Java/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String str1 = "HERO Gridman";
+        System.out.println(str1);
+        str1 = "Hello,World!";
+        System.out.println(str1);
+        int num1 = 777;
+        System.out.println(num1);
+    }
+}
