67fbad747c814b38823a6916e9133f1106950a61
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-11 14:25:55 +0000

    hanoi（仮完成）

--- js/hanoi2.js
+++ js/hanoi2.js
@@ -1,21 +1,27 @@
+let tower = {};
+tower['1本目'] = [5,4,3,2,1];
+tower['2本目'] = [];
+tower['3本目'] = [];
+
 function hanoi(n, a, b, c) {
   if (n>1) {
     hanoi(n-1,a,c,b);
-    tower('1本目','2本目','3本目');
     console.log(`${n}番目の円盤: ${a} -> ${c}`);
-    tower('1本目','2本目','3本目');
+    tower[c].push(tower[a].pop());
+    printStatus('1本目','2本目','3本目');
     hanoi(n-1,b,a,c);
   } else {
     console.log(`${n}番目の円盤: ${a} -> ${c}`);
+    tower[c].push(tower[a].pop());
+    printStatus('1本目','2本目','3本目');
   }
   
-  function tower(x,y,z) {
-    
-    console.log(`${x}: `);
-    console.log(`${y}: `);
-    console.log(`${z}: `);
+  function printStatus(x,y,z) {
+    console.log(`${x}: ${tower[x]}`);
+    console.log(`${y}: ${tower[y]}`);
+    console.log(`${z}: ${tower[z]}`);
     
   }
 }
 
-hanoi(3,'1本目','2本目','3本目');
+hanoi(5,'1本目','2本目','3本目');
