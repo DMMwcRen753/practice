9ab18039a4a06044b61479d2a37c583fe331a3c0
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-12 02:42:50 +0000

    hanoi2(完成)

--- js/hanoi2.js
+++ js/hanoi2.js
@@ -1,15 +1,18 @@
+//円盤の数を指定する
 let n = Number(prompt("円盤の数を入力してください"));
 
+// 三本の塔を作成し、それぞれの塔をリスト配列にする
 let tower = {};
 tower['1本目'] = [];
 tower['2本目'] = [];
 tower['3本目'] = [];
 
+// 最初に指定した数まで円盤を作成し、1本目の塔へ大きいものから入れていく
 for (var i=n;i>=1;i--){
-  var t = i;
-  tower['1本目'].push(t);
+  tower['1本目'].push(i);
 }
 
+// ハノイの塔を計算していく
 function hanoi(n, a, b, c) {
   if (n>1) {
     hanoi(n-1,a,c,b);
@@ -23,11 +26,11 @@ function hanoi(n, a, b, c) {
     printStatus('1本目','2本目','3本目');
   }
   
+  // 3本の塔に重なっている円盤の状態をコンソール上に出力
   function printStatus(x,y,z) {
     console.log(`${x}: ${tower[x]}`);
     console.log(`${y}: ${tower[y]}`);
     console.log(`${z}: ${tower[z]}`);
-    
   }
 }
 
