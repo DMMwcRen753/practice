4773613b0c2fcd935bafa8a999d79773b27e871c
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-12 11:22:50 +0000

    型について

---
+++ Java/Chapter05.java
@@ -0,0 +1,19 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+        long l = 10000000000000L;
+        double d = 3.14;
+        boolean b = true;
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(b);
+        Float f = 3.14f;
+        Integer i = 777;
+        Character c = 'C';
+        System.out.println(f);
+        System.out.println(i);
+        System.out.println(c);
+        int[] array = {0,10,20,30,40,};
+        System.out.println(array[3]);
+        
+    }
+}
