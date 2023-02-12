97fd47829ab11fe260548e0cb2caa952ccd993a3
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-12 02:29:40 +0000

    hanoi2(完成)

--- js/hanoi2.js
+++ js/hanoi2.js
@@ -11,15 +11,16 @@ for (var i=n;i>=1;i--){
 }
 
 function hanoi(n, a, b, c) {
-  if (n>0) {
+  if (n>1) {
     hanoi(n-1,a,c,b);
     console.log(`${n}番目の円盤: ${a} -> ${c}`);
     tower[c].push(tower[a].pop());
     printStatus('1本目','2本目','3本目');
     hanoi(n-1,b,a,c);
   } else {
-    console.log(`円盤がありません`);
-    return;
+    console.log(`${n}番目の円盤: ${a} -> ${c}`);
+    tower[c].push(tower[a].pop());
+    printStatus('1本目','2本目','3本目');
   }
   
   function printStatus(x,y,z) {
