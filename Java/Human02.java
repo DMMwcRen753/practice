748d1e079d7d946ec104d5d5e9ba94fae43f3cad
Ren Saitou <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-15 05:19:07 +0000

    package準備

---
+++ Java/Human02.java
@@ -0,0 +1,16 @@
+package classMethod.Human;
+
+public clas Human02 {
+    public String name;
+    public int age;
+    
+    public Human02() {
+        name = "山田";
+        age = 20;
+    }
+    
+    public Human02(String name, int age){
+        this.name = "佐藤";
+        this.age = 24;
+    }
+}
