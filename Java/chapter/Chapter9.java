fcd1fbc8ae646a50d9b2c49e95e2e71f9133b944
Ren saito <112914054+DMMwcRen753@users.noreply.github.com>
2023-02-26 17:31:38 +0900

    Chapter9

---
+++ Java/chapter/Chapter9.java
@@ -0,0 +1,12 @@
+public class Chapter9 {
+  public static void main(String[] args) {
+    double fuelCost = 12.5;
+
+    double fuelAmount = 55.0;
+
+    Car car = new Car(fuelCost, fuelAmount);
+
+    car.move(20);
+    System.out.println("残量は" + Car.getFuelAmount() + "です");
+  }
+}
